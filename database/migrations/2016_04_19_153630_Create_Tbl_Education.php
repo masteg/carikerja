77bb99bdd03481dee972c459f13df27ad86fc25b
masteg <pranoto.teguh@gmail.com>
2016-04-20 01:28:39 +0700

    second commit

--- database/migrations/2016_04_19_153630_Create_Tbl_Education.php
+++ database/migrations/2016_04_19_153630_Create_Tbl_Education.php
@@ -12,7 +12,21 @@ class CreateTblEducation extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('Tbl_Education', function (Blueprint $table) {
+            $table->integer('Id_account');
+            $table->increments('Id_education');
+            $table->string('Institute',20);
+            $table->date('Grad_date');
+            $table->string('Qualification',30);
+            $table->string('Institute_loc',20);
+            $table->string('Field_of_Study',50);
+            $table->string('Major',20);
+            $table->string('Grade',10);
+            $table->decimal('Score',5);
+            $table->decimal('Score_out_of',5);
+            $table->text('Add_info');
+            $table->timestamps();
+    });
     }
 
     /**
@@ -22,6 +36,6 @@ class CreateTblEducation extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('Tbl_Education');
     }
 }
